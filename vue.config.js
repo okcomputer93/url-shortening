8bdd5b1acd8121e15c89c22da01fa802864d2c80
Omar J <okcomputer2393@gmail.com>
2021-05-08 00:34:45 -0500

    Fix routes for js and css

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath:
-        process.env.NODE_ENV === "production" ? "/url-shortening/dist/" : "/",
+        process.env.NODE_ENV === "production" ? "/url-shortening/" : "/",
     css: {
         loaderOptions: {
             scss: {
